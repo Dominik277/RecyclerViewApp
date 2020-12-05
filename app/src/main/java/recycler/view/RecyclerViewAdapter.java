8398de6357610769b94d561edb3c83e83c95c48e
Dominik277 <dominikdoric277@gmail.com>
2020-12-05 20:04:14 +0100

    Popunjene tri metode u RecyclerViewAdapter klasi.

--- app/src/main/java/recycler/view/RecyclerViewAdapter.java
+++ app/src/main/java/recycler/view/RecyclerViewAdapter.java
@@ -1,7 +1,13 @@
 package recycler.view;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,23 +18,51 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
     @NonNull
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.jedan_redak,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
 
+        ViewModel drzave = mDrzave.get(position);
+
+        TextView imeDrzave = holder.imeDrzave;
+        imeDrzave.setText(drzave.getDrzava());
+
+        TextView imeKontinenta = holder.imeKontinenta;
+        imeKontinenta.setText(drzave.getKontinent());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDrzave.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView imeDrzave;
+        public TextView imeKontinenta;
+        public ImageView slika;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imeDrzave = itemView.findViewById(R.id.imeDrzave);
+            imeKontinenta = itemView.findViewById(R.id.imeKontinenta);
+            slika = itemView.findViewById(R.id.slika);
         }
     }
+
+    private List<ViewModel> mDrzave;
+    public RecyclerViewAdapter(List<ViewModel> drzave){
+        this.mDrzave = drzave;
+    }
+
 }
